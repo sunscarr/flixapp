c7375e0e633ee40e1a93427b187185525c13b5af
sunscarr <gyawali.sans@gmail.com>
2020-01-20 16:59:53 -0800

    Initial Commit

---
+++ flixapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixapp
+//
+//  Created by Sanskar Gyawali on 1/20/20.
+//  Copyright © 2020 Sanskar Gyawali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
