d8568e20868c3c2c2c75021f7fff0b8bbbb3ac9b
sunscarr <gyawali.sans@gmail.com>
2020-01-26 18:39:18 -0800

    Finished part 2

---
+++ flixapp/MovieDetailsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MovieDetailsViewController.swift
+//  flixapp
+//
+//  Created by Sanskar Gyawali on 1/26/20.
+//  Copyright © 2020 Sanskar Gyawali. All rights reserved.
+//
+import AlamofireImage
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        
+        let posterPath = movie["poster_path"] as! String
+        
+        let posterUrl = URL(string: baseURL + posterPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropUrl!)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flixapp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixapp
+//
+//  Created by Sanskar Gyawali on 1/26/20.
+//  Copyright © 2020 Sanskar Gyawali. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
